ee3e1755f49f0c118252b3eff4a5710327d0fd3b
Макс <0w1.go.0w1@gmail.com>
2022-02-19 22:41:06 +0300

    Добавил проверку на положительное число. Чтобы максимальное число не было равно минимальному. И чтобы минимальное число было меньше максимального

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // Функция для получения случайного положительного целого числа из диапозона
 function getRandomNumber(min, max) {
+	if (min < 0) {
+  	return console.log('Минимальное число меньше нуля!')
+  }
+
+  if (min > max || min === max) {
+  	return console.log('Минимальное число больше или равно максимальному!')
+  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
 
@@ -7,4 +15,7 @@ function getRandomNumber(min, max) {
 }
 
 getRandomNumber(0, 2);
+getRandomNumber(-2, 2);
+getRandomNumber(2, 2);
+getRandomNumber(3, 2);
 
