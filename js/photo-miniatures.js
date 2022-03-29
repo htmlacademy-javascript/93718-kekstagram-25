331d30b926ed0bac7f5e71c7302ecf994da03761
Макс <0w1.go.0w1@gmail.com>
2022-03-29 21:52:34 +0300

    Рефакторинг кода. Изменил объявление шаблона для лучшей читабельности

--- js/photo-miniatures.js
+++ js/photo-miniatures.js
@@ -1,7 +1,9 @@
 import {generateData} from './data.js';
 
 const picturesContainer = document.querySelector('.pictures');
-const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
 const fragment = document.createDocumentFragment();
 
 const miniaturesData = generateData();
