983c882255a4225ea1c429c4c62249080d19c2b5
Макс <0w1.go.0w1@gmail.com>
2022-03-29 20:54:40 +0300

    Добавляем данные url, кол-во лайков и комментариев в разметку шаблона и после в коробку fragment

--- js/photo-miniatures.js
+++ js/photo-miniatures.js
@@ -6,3 +6,11 @@ const fragment = document.createDocumentFragment();
 
 const miniaturesData = generateData();
 
+miniaturesData.forEach(({url, likes, comments}) => {
+  const elementPicture = picturesTemplate.cloneNode(true);
+
+  elementPicture.querySelector('.picture__img').src = url;
+  elementPicture.querySelector('.picture__likes').textContent = likes;
+  elementPicture.querySelector('.picture__comments').textContent = comments.length;
+  fragment.appendChild(elementPicture);
+});
