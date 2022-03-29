0da8d8f4a80f78f8c551c6a499ebfa5e302584b0
Макс <0w1.go.0w1@gmail.com>
2022-03-29 20:52:03 +0300

    Добавил коробку fragment и определил сгенерированные данные для миниатюр

--- js/photo-miniatures.js
+++ js/photo-miniatures.js
@@ -2,4 +2,7 @@ import {generateData} from './data.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+
+const miniaturesData = generateData();
 
