bb06144b864d5a551ba2f9976f2ebd003350756f
Макс <0w1.go.0w1@gmail.com>
2022-03-29 20:46:36 +0300

    Добавил модуль для миниатюр. Определил контейнер для миниатюр и шаблон

---
+++ js/photo-miniatures.js
@@ -0,0 +1,5 @@
+import {generateData} from './data.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
