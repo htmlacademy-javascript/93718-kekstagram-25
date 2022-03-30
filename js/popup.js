9e21f7b2d5b831c9a8e6eeec998f2b853583a463
Макс <0w1.go.0w1@gmail.com>
2022-03-30 18:55:48 +0300

    Добавил функцию для добавления комментариев

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,23 @@ const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
 const commentsCount = bigPicture.querySelector('.comments-count');
 const pictureDescription = bigPicture.querySelector('.social__caption');
+const commentsList = bigPicture.querySelector('.social__comments');
+const commentItem = commentsList.querySelector('.social__comment');
+
+const showComments = (element) => {
+  commentsList.innerHTML = '';
+
+  element.forEach((comment) => {
+    const commentElement = commentItem.cloneNode(true);
+
+    commentElement.querySelector('.social__picture').alt = comment.name;
+    commentElement.querySelector('.social__text').textContent = comment.message;
+    commentElement.querySelector('.social__picture').src = comment.avatar;
+
+    commentsList.appendChild(commentElement);
+  });
+};
+
 
 const showBigPicture = ({url, likes, comments, description}) => {
   bigPicture.classList.remove('hidden');
@@ -11,6 +28,7 @@ const showBigPicture = ({url, likes, comments, description}) => {
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
   pictureDescription.textContent = description;
+  showComments(comments);
 };
 
 export {showBigPicture};
