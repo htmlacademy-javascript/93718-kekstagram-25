dfbc179962fe461ab1f61a3f54abbfc69e880a0e
Макс <0w1.go.0w1@gmail.com>
2022-03-31 22:44:35 +0300

    Написал функцию для закрытия формы по крестику и кнопке

--- js/form.js
+++ js/form.js
@@ -1,13 +1,32 @@
+import {isEscapeKey} from './util.js';
+
 const body = document.querySelector('body');
 const form = body.querySelector('.img-upload__form');
 const imgUploadInput = form.querySelector('.img-upload__input');
 const imgUploadOverlay = form.querySelector('.img-upload__overlay');
-// const imgUploadCancel = form.querySelector('.img-upload__cancel');
+const imgUploadCancel = form.querySelector('.img-upload__cancel');
+
+const closeForm =  () => {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', closeEscapeButton);
+};
+
+function closeEscapeButton (evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+
+    closeForm();
+  }
+}
 
 const openForm = () => {
   imgUploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
+
+  imgUploadCancel.addEventListener('click', closeForm);
+  document.addEventListener('keydown', closeEscapeButton);
 };
 
 imgUploadInput.addEventListener('change', openForm);
-
