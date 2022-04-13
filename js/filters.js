212d49d7d76b83f3bc5d6f03d67d56397897868e
Макс <0w1.go.0w1@gmail.com>
2022-04-13 14:10:46 +0300

    Добавил событие для переключения кнопок фильтра в активное состояние

--- js/filters.js
+++ js/filters.js
@@ -2,8 +2,21 @@ const filter = document.querySelector('.img-filters');
 const filtersForm = filter.querySelector('.img-filters__form');
 const filtersButton = filtersForm.querySelectorAll('.img-filters__button');
 
+const filtersButtonShowHandler = (evt) => {
+  evt.preventDefault();
+
+  if (!evt.target.className.match('img-filters__button--active') && evt.target.className.match('img-filters__button')) {
+    const arrayButtons = Array.from(filtersButton);
+
+    arrayButtons.forEach((button) => button.classList.remove('img-filters__button--active'));
+
+    evt.target.classList.add('img-filters__button--active');
+  }
+};
+
 const showFiltersForm = () => {
   filter.classList.remove('img-filters--inactive');
+  filtersForm.addEventListener('click', filtersButtonShowHandler);
 };
 
 export {showFiltersForm};
