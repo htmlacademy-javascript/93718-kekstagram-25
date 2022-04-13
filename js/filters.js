714a5e7cfbcc21b689bdefc988284322c9ecb92d
Макс <0w1.go.0w1@gmail.com>
2022-04-13 19:46:14 +0300

    Создал функцию для отрисовки фильтра со случайными фотографиями

--- js/filters.js
+++ js/filters.js
@@ -1,6 +1,18 @@
 import {arrayPhotosData} from './api.js';
+import {createRandomId} from './util.js';
 
-import {renderPhotoMiniatures} from './photo-miniatures.js';
+import {
+  renderPhotoMiniatures,
+  picturesContainer,
+  picturesTemplate,
+  fragment
+} from './photo-miniatures.js';
+
+import {bigPictureShow} from './popup.js';
+
+const RANDOM_PHOTOS_COUNT = 10;
+const MIN_ID_COUNT = 0;
+const MAX_ID_COUNT = 24;
 
 const filter = document.querySelector('.img-filters');
 const filtersForm = filter.querySelector('.img-filters__form');
@@ -17,6 +29,39 @@ const clearPictures = () => {
 const filterDefault = () => {
   clearPictures();
   renderPhotoMiniatures(arrayPhotosData);
+};
+
+const filterRandom = (photosData) => {
+  clearPictures();
+
+  const getRandomId = createRandomId(MIN_ID_COUNT, MAX_ID_COUNT);
+  const idArray = [];
+
+  for (let i = 0; i < RANDOM_PHOTOS_COUNT; i++) {
+    idArray[i] = getRandomId();
+  }
+
+  idArray.forEach((id) => {
+    photosData.forEach((photo) => {
+      if (id === photo.id) {
+        const elementPicture = picturesTemplate.cloneNode(true);
+
+        elementPicture.querySelector('.picture__img').src = photo.url;
+        elementPicture.querySelector('.picture__likes').textContent = photo.likes;
+        elementPicture.querySelector('.picture__comments').textContent = photo.comments.length;
+
+        elementPicture.addEventListener('click', (evt) => {
+          evt.preventDefault();
+
+          bigPictureShow(photo);
+        });
+
+        fragment.appendChild(elementPicture);
+      }
+    });
+  });
+
+  picturesContainer.appendChild(fragment);
 };
 
 const filtersButtonShowHandler = (evt) => {
